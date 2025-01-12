285afef960786d2aced28f89684f4f340c994c1e
Thiago Menezes <thiagomvas@gmail.com>
2025-01-12 12:12:16 -0300

    feat: Add Navigation

--- src/Meminisse.Desktop/ViewModels/MainWindowViewModel.cs
+++ src/Meminisse.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,34 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Meminisse.Desktop.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     [ObservableProperty] private ViewModelBase? _selectedViewModel = new HomeViewModel();
+    
+    public ObservableCollection<NavigationItem> NavigationItems { get; } = new ObservableCollection<NavigationItem>
+    {
+        new NavigationItem("Home", new HomeViewModel()),
+        new NavigationItem("Settings", new SettingsViewModel())
+    };
+    
+    [RelayCommand]
+    public void SelectViewModel(ViewModelBase viewModel)
+    {
+        SelectedViewModel = viewModel;
+    }
+}
+
+public class NavigationItem
+{
+    public string Name { get; set; }
+    public ViewModelBase ViewModel { get; set; }
+    
+    public NavigationItem(string name, ViewModelBase viewModel)
+    {
+        Name = name;
+        ViewModel = viewModel;
+    }
 }
